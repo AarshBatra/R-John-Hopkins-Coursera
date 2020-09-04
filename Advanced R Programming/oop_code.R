54a94dbf206fdb7ee23df24c474f0f8b33699043
AarshBatra <aarshbatra.in@gmail.com>
2020-09-04 16:52:26 +0530

    commenting oop.R file, still incomplete

--- Advanced R Programming/oop_code.R
+++ Advanced R Programming/oop_code.R
@@ -1,4 +1,4 @@
-# Assignment Part 2: Longitudnal Data class and methods------------------------
+# Assignment Part 2: Longitudnal Data class and methods (using S3 system)------
 
 
 # packages---------------------------------------------------------------------
@@ -17,15 +17,7 @@ library(methods)
 # read in data-----------------------------------------------------------------
 longData <- read_csv("MIE.csv")
 
-# using S3 system create a longitudnal data object
-make_LD <- function(dataObj){
-  structure(dataObj, class = c("LongitudnalData", "data.frame"))
-}
-
-print.LongitudnalData <- function(longDataObj){
-  paste("Longitudnal Dataset with", length(unique(longDataObj$id)), "subjects.")
-  
-}
+# creating a few generic methods 
 
 subject <- function(dataObj, id){
   UseMethod("subject")
@@ -39,11 +31,26 @@ room <- function(subjObj, roomNum){
   UseMethod("room")
 }
 
+
+# convert a data frame into a longitudnal data object
+make_LD <- function(dataObj){
+  structure(dataObj, class = c("LongitudnalData", "data.frame"))
+}
+
+# function definition: print method for the objects of class "LongitudnalData"
+print.LongitudnalData <- function(longDataObj){
+  paste("Longitudnal Dataset with", length(unique(longDataObj$id)), "subjects.")
+  
+}
+
+# function definition: subject method for the longitudnal data object
+# returns an object of class "subject"
 subject.LongitudnalData <- function(dataObj, id){
  objToRet <- list(data = dataObj, id = id)
  structure(objToRet, class = c("subject"))
 }
 
+# function definition: print method for the object of class "subject"
 print.subject <- function(subjObj){
   extractId <- subjObj[["id"]]
   if(extractId %in% subjObj[["data"]]$id){
@@ -54,6 +61,8 @@ print.subject <- function(subjObj){
   
 }
 
+# function definition: summary method for the object of class "subject"
+# returns an object of class "summary"
 summary.subject <- function(subjObj){
   extractId <- subjObj[["id"]]
   extractData <- subjObj[["data"]]
@@ -69,25 +78,32 @@ summary.subject <- function(subjObj){
   structure(retSummList, class = c("summary"))
 }
 
+# function definition: print method for the object of class "summary"
 print.summary <- function(summaryDataObj){
   print(summaryDataObj[["summData"]])
-  
 }
-  
+
+# function definition: visit method for the object of class "subject"
+# returns an object of class "visit"
 visit.subject <- function(subjObj, visitNum){
   objToRet <- list(dataList = subjObj, visNum = visitNum)
   structure(objToRet, class = c("visit"))
 }
 
+
+# function definition: room method for the object of class "visit"
+# returns an object of class "room"
 room.visit <- function(visObj, roomNum){
   objToRet <- list(visObjList = visObj, roomNum = roomNum)
   structure(objToRet, class = c("room"))
 }
 
+# function definition: print method for the object of class "room"
 print.room <- function(x){
   
 }
 
+# function definition: summary method for the object of class "room"
 summary.room <- function(x){
    
 }
